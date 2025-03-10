7373e9c3c250df277339444141a2335c9406f42c
Philipp Schuster <philipp.schuster1@alumni.fh-aachen.de>
2025-03-10 13:03:08 +0100

    Dein Commit-Text

--- Project.py
+++ Project.py
@@ -1,1 +1,15 @@
-print("Ixh werde euch alle besiegen!")
+print("Ihr werdet mich niemals besiegen!")
+eingabe = input("Gebe +,-,*,/ an: ")
+a = float(input("Zahl 1: "))
+b = float(input("Zahl 2: "))
+
+if eingabe=="+":
+    print(a+b)
+elif eingabe == "-":
+    print(a-b)
+elif eingabe == "*":
+    print(a*b)
+elif eingabe == "/":
+    print(round(a/b,2))
+else:
+    print("Falsche eingabe")
