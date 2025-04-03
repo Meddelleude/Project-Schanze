b9b2c7f72efd1ab9fc460115ee4d20b9153cc012
Philipp Schuster <philipp.schuster1@alumni.fh-aachen.de>
2025-04-03 13:10:47 +0200

    forecasting and cleanup

--- my_darts_script.py
+++ my_darts_script.py
@@ -5,7 +5,9 @@ from darts.models import NBEATSModel
 from darts.metrics import mae, rmse
 from darts.dataprocessing.transformers import Scaler
 import pytorch_lightning as pl
+from datetime import datetime
 
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 # Daten laden
 df = pd.read_csv("data_set_1.csv", parse_dates=["timestamp"])
 
@@ -21,15 +23,15 @@ train, val = series.split_after(0.8)
 
 # Early Stopping konfigurieren
 early_stopping = pl.callbacks.EarlyStopping(
-    monitor="val_loss", patience=20, mode="min"
+    monitor="val_loss", patience=5, mode="min"
 )
 
 # NBEATS-Modell initialisieren
 model = NBEATSModel(
-    input_chunk_length=1500,
-    output_chunk_length=200,
+    input_chunk_length=1400,
+    output_chunk_length=300,
     random_state=42,
-    pl_trainer_kwargs={"callbacks": [early_stopping],"accelerator": "gpu","devices": 1},
+    pl_trainer_kwargs={"callbacks": [early_stopping]},
 )
 
 # Modell trainieren
@@ -38,18 +40,24 @@ model.fit(train, val_series=val, epochs=50, verbose=True)
 # Vorhersage erzeugen
 forecast = model.predict(n=1756)
 forecast = scaler.inverse_transform(forecast)  # Zurückskalieren
+Mae = mae(val, model.predict(len(val)))
+RMSE = rmse(val, model.predict(len(val)))
 
 # Plots erstellen
-fig, ax = plt.subplots()
-series2.plot(label="Echte Werte", ax=ax)
-forecast.plot(label="Vorhersage", ax=ax)
+fig, ax = plt.subplots(figsize=(16, 8))
+series2.plot(label="Echte Werte", ax=ax,linewidth=0.5)
+forecast.plot(label="Vorhersage", ax=ax,linewidth=0.5)
 ax.legend()
-plt.savefig("forecast_plot.png")  # Speichert den Plot
+ax.set_xlabel("Zeit", fontsize=12)
+ax.set_ylabel("Messwerte", fontsize=12)
+plt.legend()
+plt.title(f"forecast_plot_{timestamp},Mae:  {Mae}, RMSE: {RMSE},ICL: {model.input_chunk_length}, OCL: {model.output_chunk_length} ")
+plt.savefig(f"Forecast/forecast_plot_{timestamp}.png")  # Speichert den Plot
 plt.show()
 
 # Vorhersage als CSV speichern
 forecast_df = forecast.pd_dataframe()
-forecast_df.to_csv("forecast_data.csv", index=True)
+forecast_df.to_csv(f"Forecast/forecast_data_{timestamp}.csv", index=True)
 
 # Fehler berechnen
 print("MAE:", mae(val, model.predict(len(val))))
--- test.py
+++ test.py
@@ -1,2 +1,4 @@
 import torch
-print(torch.cuda.is_available())
+print(torch.cuda.is_available())  # Sollte `True` zurückgeben
+print(torch.cuda.device_count())  # Sollte >= 1 sein
+print(torch.cuda.get_device_name(0))  # Sollte deine AMD GPU anzeigen
