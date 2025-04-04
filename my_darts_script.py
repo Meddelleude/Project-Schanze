68d956ac45721dd946cb3812faec31ef3f9d7879
Philipp Schuster <philipp.schuster1@alumni.fh-aachen.de>
2025-04-04 12:07:11 +0200

    covariates added

--- my_darts_script.py
+++ my_darts_script.py
@@ -14,11 +14,15 @@ df = pd.read_csv("data_set_1.csv", parse_dates=["timestamp"])
 # Zeitreihen-Objekt erstellen
 series = TimeSeries.from_dataframe(df, "timestamp", "meter_reading")
 series2 = TimeSeries.from_dataframe(df, "timestamp", "meter_reading")
+covariate_series = TimeSeries.from_dataframe(df, "timestamp", "anomaly")
+
 # Skalierung anwenden
 scaler = Scaler()
+covariate_scaled = scaler.fit_transform(covariate_series)
 series = scaler.fit_transform(series)
 
 # Trainings- und Testdaten aufteilen
+train_cov, val_cov = covariate_series.split_after(0.8)
 train, val = series.split_after(0.8)
 
 # Early Stopping konfigurieren
@@ -28,20 +32,27 @@ early_stopping = pl.callbacks.EarlyStopping(
 
 # NBEATS-Modell initialisieren
 model = NBEATSModel(
-    input_chunk_length=1400,
-    output_chunk_length=300,
+    input_chunk_length=1000,
+    output_chunk_length=200,
     random_state=42,
     pl_trainer_kwargs={"callbacks": [early_stopping]},
 )
 
 # Modell trainieren
-model.fit(train, val_series=val, epochs=50, verbose=True)
+model.fit(
+    series=train,
+    past_covariates=train_cov,
+    val_series=val,
+    val_past_covariates=val_cov,
+    epochs=1,
+    verbose=True
+)
 
 # Vorhersage erzeugen
-forecast = model.predict(n=1756)
-forecast = scaler.inverse_transform(forecast)  # Zurückskalieren
-Mae = mae(val, model.predict(len(val)))
-RMSE = rmse(val, model.predict(len(val)))
+forecast = model.predict(n=len(val), past_covariates=covariate_scaled)
+forecast = scaler.inverse_transform(forecast)
+Mae = mae(val, model.predict(len(val),past_covariates=covariate_scaled))
+RMSE = rmse(val, model.predict(len(val),past_covariates=covariate_scaled))
 
 # Plots erstellen
 fig, ax = plt.subplots(figsize=(16, 8))
@@ -60,5 +71,5 @@ forecast_df = forecast.pd_dataframe()
 forecast_df.to_csv(f"Forecast/forecast_data_{timestamp}.csv", index=True)
 
 # Fehler berechnen
-print("MAE:", mae(val, model.predict(len(val))))
-print("RMSE:", rmse(val, model.predict(len(val))))
+print("MAE:", mae(val, model.predict(len(val),past_covariates=covariate_scaled)))
+print("RMSE:", rmse(val, model.predict(len(val),past_covariates=covariate_scaled)))
